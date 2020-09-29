234aba459e0f3ed2d15292c6b347333bb637bdd8
André Philipsson <62261582+andrephilipsson@users.noreply.github.com>
2020-09-29 21:24:25 +0200

    chore(install): add scala

--- bin/install.sh
+++ bin/install.sh
@@ -23,12 +23,20 @@ sudo apt install -y \
 	tmux \
 	unzip \
 	zsh \
+	default-jre \
+	scala
 
 # Install Github-cli
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
 sudo apt install -y gh
+
+# Install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt update
+sudo apt install sbt
 
 # Create directories
 mkdir Projects
