7909c6b96f2cf76287152d75c9a67b8b70c0a9ab
André Philipsson <62261582+andrephilipsson@users.noreply.github.com>
2021-03-14 10:27:10 +0100

    feat(vim): port config to lua (WIP)

--- .config/nvim/lua/plugin/completion-nvim.lua
+++ .config/nvim/lua/plugin/completion-nvim.lua
@@ -1,8 +1,8 @@
 local inoremap = vim.keymap.inoremap
 
 -- Use <Tab> and <S-Tab> to navigate through popup menu
--- inoremap { "<Tab>", "pumvisible() ? '<C-n>' : '<Tab>'", { expr = true } }
--- inoremap { "<S-Tab>", "pumvisible() ? '<C-p>' : '<S-Tab>'", { expr = true } }
+inoremap { "<Tab>", "pumvisible() ? ' '\\<C-n>' : '\\<Tab>'", { expr = true } }
+inoremap { "<S-Tab>", "pumvisible() ? '\\<C-p>' : '\\<S-Tab>'", { expr = true } }
 
 -- Auto insert parenthesis when completing functions and methods
 vim.g.completion_enable_auto_paren = 1
