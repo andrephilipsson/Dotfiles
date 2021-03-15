637aab4b8cdd37d22869ab34b41977a301bd6159
André Philipsson <62261582+andrephilipsson@users.noreply.github.com>
2021-03-15 21:50:59 +0100

    fix(vim): fix <tab>
    Smart tab complete triggered the completion if there was text before the
    cursor. So tab would only work on empty lines, which I obviously don't
    want.

--- .config/nvim/lua/plugin/completion-nvim.lua
+++ .config/nvim/lua/plugin/completion-nvim.lua
@@ -1,5 +1,6 @@
-vim.cmd('imap <tab> <Plug>(completion_smart_tab)')
-vim.cmd('imap <s-tab> <Plug>(completion_smart_s_tab)')
+-- Use <Tab> and <S-Tab> to navigate through popup menu
+vim.cmd('inoremap <expr> <Tab>   pumvisible() ? "\\<C-n>" : "\\<Tab>"')
+vim.cmd('inoremap <expr> <S-Tab> pumvisible() ? "\\<C-p>" : "\\<S-Tab>"')
 
 -- Auto insert parenthesis when completing functions and methods
 vim.g.completion_enable_auto_paren = 1
