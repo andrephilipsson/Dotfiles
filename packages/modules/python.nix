9be70907b7fdbf681b84b9ea05615f03794b3128
André Philipsson <62261582+andrephilipsson@users.noreply.github.com>
2021-06-26 21:31:52 +0200

    chore: cleanup python module
    I have no need to install python packages globally (yet), so instead I
    use the built in `venv` module (`python -m venv`), to create virtual
    environments for every project. This is quite nice since we then don't
    have to rely on old python packages available in the nix packages.

--- packages/modules/python.nix
+++ packages/modules/python.nix
@@ -1,8 +1,6 @@
 { config, pkgs, libs, ... }:
 {
   home.packages = with pkgs; [
-    (python3.withPackages (ps: with ps; [ pip virtualenv ])) # TODO: Make sure this works
-    # TODO: install python langauge server here:
-    # pip install 'python-language-server[all]'
+    python3
   ];
 }
