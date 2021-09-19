fbc50f323a6d3964fc3d64193a26b866857e461b
André Philipsson <andre@andrephilipsson.com>
2021-09-19 10:51:08 +0200

    chore(nvim): remove unused snippets

--- config/nvim/lua/ape/snips/init.lua
+++ config/nvim/lua/ape/snips/init.lua
@@ -1,42 +1,15 @@
-local function prequire(...)
-  local status, lib = pcall(require, ...)
-  if (status) then return lib end
-  return nil
-end
-
-local luasnip = prequire('luasnip')
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
-end
-
-_G.tab_complete = function()
-  if require("luasnip").expand_or_jumpable() then
-    return t "<cmd>lua require'luasnip'.jump(1)<Cr>"
-  elseif vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-
-_G.s_tab_complete = function()
-  if require("luasnip").jumpable(-1) then
-    return t "<cmd>lua require'luasnip'.jump(-1)<CR>"
-  elseif vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local l = require("luasnip.extras").lambda
+local r = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local types = require("luasnip.util.types")
