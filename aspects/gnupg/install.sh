87cc50ebcb23404551260a0b3ea97b6da1ddc5b5
André Philipsson <andre@philipsson.me>
2022-12-16 22:57:39 +0100

    Rotate GPG keys

--- aspects/gnupg/install.sh
+++ aspects/gnupg/install.sh
@@ -3,7 +3,7 @@
 [ ! -d "$HOME/.gnupg" ] && mkdir ~/.gnupg
 
 if ! [ -e $HOME/.gnupg/gpg-agent.conf ]; then
-  ln -sf $(pwd)/files/gpg-agent.confg ~/.gnupg/gpg-agent.conf
+  ln -sf $(pwd)/files/gpg-agent.conf ~/.gnupg/gpg-agent.conf
   echo "Symlinked gnupg config files to ~/.gnupg"
 else
   echo "Gnupg config files already exist in ~/.gnupg"
