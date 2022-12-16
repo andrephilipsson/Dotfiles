132a0cdc5d9fffc7fdf8b2590ccc7c18ee05595b
André Philipsson <andre@philipsson.me>
2022-12-16 12:17:33 +0100

    Add git config install script

---
+++ aspects/git/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if ! [ -e $HOME/.config/git ]; then
+  ln -sf $(pwd)/files ~/.config/git
+  echo "Symlinked git config files to ~/.config/git"
+else
+  echo "Git config files already exist in ~/.config/git"
+fi
