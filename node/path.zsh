004c4f09d42fd44b89568f0f63ddd4fe5976525e
André Philipsson <andre@philipsson.me>
2023-09-02 19:07:57 +0200

    Updates

--- node/path.zsh
+++ node/path.zsh
@@ -1,7 +1,4 @@
 eval "$(fnm env --use-on-cd)"
 
 export PNPM_HOME="/Users/ape/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
+export PATH="$PNPM_HOME:$PATH"
