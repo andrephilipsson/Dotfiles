adb1d8973bf4be76393f87272fee5c6c8a024ebe
André Philipsson <andre@philipsson.me>
2023-10-11 16:39:57 +0200

    Start over

--- nvim/config/nvim.symlink/after/plugin/git.lua
+++
@@ -1,7 +0,0 @@
-local has_gitsigns, gitsigns = pcall(require, "gitsigns")
-
-if not has_gitsigns then
-	return
-end
-
-gitsigns.setup({})
--- nvim/config/nvim.symlink/init.lua
+++
@@ -1,9 +0,0 @@
--- Disable built-in plugins
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-
--- Custom config
-require("ape.mappings")
-require("ape.plugins")
